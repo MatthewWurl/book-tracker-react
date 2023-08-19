d4c14ad113a73ff0e2ce23ca04f4521d88560c57
Matt <56481807+AnxietyMedicine@users.noreply.github.com>
2023-08-18 20:17:59 -0400

    Create header, library, and wishlist components & set up router

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Library from "./components/Library";
+import WishList from "./components/WishList";
+
 export default function App() {
-  return <></>;
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/library" element={<Library />} />
+        <Route path="/wishlist" element={<WishList />} />
+      </Routes>
+    </Router>
+  );
 }
